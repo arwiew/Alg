1e17a1e9456de31e5c9e6484028bc3b04e5dd61b
arwiew <pocht1991@yandex.ru>
2019-06-15 17:12:31 +0300

    ДЗ № 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,57 @@
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
+
 public class Main {
 
     public static void main(String[] args) {
-        Array<Integer> array = new ArrayImpl<>(8);
+
+
+        final int ARR_SIZE = 80000;
+
+        Array<Integer> arr = new ArrayImpl<>(ARR_SIZE);
+                for (int i = 0; i < ARR_SIZE ; i++) {
+                    int random = randomGen(1,ARR_SIZE);
+                    arr.add(random);
+
+        }
+        Array<Integer> arr1 = arr;
+        Array<Integer> arr2 = arr;
+        Array<Integer> arr3 = arr;
+
+      timer(arr1,arr2,arr3);
+
+    }
+
+    private static void timer(Array<Integer> arr1, Array<Integer> arr2, Array<Integer> arr3){
+        long startBubble = System.currentTimeMillis();
+        arr1.sortBubble();
+        long stopBubble = System.currentTimeMillis()-startBubble;
+        System.out.println("Bubble time : " + stopBubble);
+
+        long startInserrt = System.currentTimeMillis();
+        arr2.sortInsert();
+        long stopInsert = System.currentTimeMillis()-startInserrt;
+        System.out.println("Insert time : " + stopInsert);
+
+        long startSelect = System.currentTimeMillis();
+        arr3.sortSelect();
+        long stopSelect = System.currentTimeMillis()-startSelect;
+        System.out.println("Select time : " + stopSelect);
+
+    }
+
+    private static int randomGen(int i, int i1) {
+        int max = i;
+        int min = i1;
+        int x = (int)(Math.random()*((max-min)+1))+min;
+        return x;
+    }
+
+
+}
+
+  /*      Array<Integer> array = new ArrayImpl<>(8);
         //Array<Integer> array = new SortedArrayImpl<>(8);
         array.add(1);
         array.add(5);
@@ -40,69 +87,29 @@ public class Main {
 
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*  private static double[] arr;
-    private static int addSize = 1;
-    private static int deleteSize;
-    private static int arrSize = 10;
-
-
-    public static void main(String[] args) {
-
-        arr = new double[arrSize];
-        for (int i = 0; i<arr.length; i++){
-            arr[i] = rnd();
-        }
-        System.out.println(Arrays.toString(arr));
-        deleteSize = 1;
-
-    }
-
-    private static double rnd(){
-        double rnd = (int) Math.random();
-        return rnd;
-    }
-
-
-    private void add(){
-        int add = 1;
-        arr = new double[arrSize + addSize];
-        arr[arrSize+add] = add;
-        System.out.println(Arrays.toString(arr));
-    }
-
-    private static void delete(){
-        arr = new double[arrSize - deleteSize];
-        System.out.println(Arrays.toString(arr));
-    }
-
-    private void find(){
-
-        System.out.println(Arrays.toString(arr));
-    }
 */
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
